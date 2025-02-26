9d41c09a815dc279cc46a1a6a67946d56dae86fb
aya-zid <azid28278@gmail.com>
2025-02-26 11:46:41 +0100

    fix blank lines again

--- hello-devops/test_app.py
+++ hello-devops/test_app.py
@@ -4,13 +4,12 @@ from app import app
 
 
 class TestApp(unittest.TestCase):
-    
-    
+
     def test_home_route(self):
         tester = app.test_client(self)
         response = tester.get('/')
-        self.assertEqual(response.status_code, 200)  
-        self.assertEqual(response.data.decode('utf-8'), "Hello, CI/CD!")  
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode('utf-8'), "Hello, CI/CD!")
 
 
 if __name__ == '__main__':
